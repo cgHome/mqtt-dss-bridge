8772dc82d1a8129f2ca1e9c8e92d555fc4e690bc
cgHome <cgHome@gmx.net>
2015-06-15 09:21:29 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mqtt-dss-bridge
 MQTT digitalSTROM-Server Bridge
+
+mqtt-dss-bridge is currently in **Developer Preview**.
+
+## How to Install
+
+To get started, just install the NPM module:
+
+    $ npm install --save mqtt-dss-bridge
+    
+## Configuration
+
+See : [config](config.js)
+
+
+## License
+
+[MIT License](LICENSE)
+
--- config.js
+++ config.js
@@ -1,16 +1,22 @@
 /*
-*   Configuration
-*/
+ *   Configuration
+ */
+
+// ToDo: Temp dSS using a self-signed certificate
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
 module.exports = {
-    // DigitalSTROM-Server
-    dssUrl: 'https://dss.local:8080',
-    // Obtaining dssAppToken: https://dss.local:8080/json/system/requestApplicationToken?applicationName=mqttBridge
-    dssAppToken: 'e840aba0336717f7d4b4d9c7d638d055a5308cfde99b54cccb143175cac48d9c',
-
-    // MQTT-Client
-    brokerUrl: 'mqtt://localhost:1883',
-    baseTopic: 'dSS',
-
     // Internal
     baseRefreshInterval: 5000,
+
+    broker: {
+        url: 'mqtt://localhost:1883',
+        baseTopic: 'dss',
+    },
+    dss: {
+        // DigitalSTROM-Server
+        url: 'https://dss.local:8080',
+        // Obtaining dssAppToken: https://dss.local:8080/json/system/requestApplicationToken?applicationName=mqttBridge
+        appToken: 'e840aba0336717f7d4b4d9c7d638d055a5308cfde99b54cccb143175cac48d9c',
+    }
 };
